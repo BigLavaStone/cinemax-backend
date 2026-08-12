1958b59f5682c41c0e64f30ba39f330ce5e623de
Mrityunjoy Sahoo <148135986+BigLavaStone@users.noreply.github.com>
2026-08-13 00:56:49 +0530

    v1.1

--- models/Hall.js
+++ models/Hall.js
@@ -1,23 +1,23 @@
 import mongoose  from "mongoose";
 
-// Schema for an individual seat
+
 const seatSchema = new mongoose.Schema({
     rowNo: { type: Number, required: true },
     colNo: { type: Number, required: true },
     rowLbl: { type: String, required: true },
-    colLbl: { type: String, required: true }, // Kept as String in case of labels like '1A'
+    colLbl: { type: String, required: true }, 
     lbl: { type: String, required: true },
-    type: { type: String, default: 'seat' } // can be 'seat' or null
+    type: { type: String, default: 'seat' } 
 }, { _id: false });
 
-// Schema for a row of seats
+
 const rowSchema = new mongoose.Schema({
     rowNo: { type: Number, required: true },
     rowLbl: { type: String, required: true },
     seats: [seatSchema]
 }, { _id: false });
 
-// Main Hall schema
+
 const hallSchema = new mongoose.Schema({
     hallName: { type: String, required: true },
     location: { type: String, required: true },
